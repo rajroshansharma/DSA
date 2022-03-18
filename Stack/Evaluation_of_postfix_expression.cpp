eab52c8f22ecc8a098f12cf26dc3fa1afba0b906
raz <48359006+rajroshansharma@users.noreply.github.com>
2022-03-18 11:47:40 +0530

    Update Evaluation_of_postfix_expression.cpp
    added notes and algorithm and references for update

--- Stack/Evaluation_of_postfix_expression.cpp
+++ Stack/Evaluation_of_postfix_expression.cpp
@@ -98,3 +98,40 @@ int main()
 
     return 0;
 }
+
+/*
+OUTPUT
+input : 32+
+ouptput : 5
+
+Explantion
+push(3)
+push(2)
+a = pop()
+b = pop()
+c = a + b
+push(c)
+output print c
+
+Algorithm 
+1. If symbol is digit then push. 
+2. Else pop 2 times and store them in two variables. 
+3. perform operations on two variables and store in variable 3 
+4. push variable 3 
+
+Notes
+1. In line number 63 we can see that there is a range of digits which are in character data type. the function is returning 0 for false and 1 for true. 
+2. To change character from int data type we can write like character_variable - '0' 
+3. In C++ there is no functions four strings we need to access those things by using the class method. for example postfix.length() 
+
+
+
+upcoming alogrthm
+drawback -> can only calculate for single digits
+sulution add space in that string.
+refrence -> https://algorithms.tutorialhorizon.com/evaluation-of-postfix-expressions-polish-postfix-notation-set-2/#:~:text=Postfix%20notation%20is%20a%20notation,operands%20appear%20before%20their%20operators.&text=Operands%20are%20real%20numbers%20(could%20be%20multiple%20digits).&text=Blanks%20are%20used%20as%20a%20separator%20in%20expression.
+
+
+
+
+*/
